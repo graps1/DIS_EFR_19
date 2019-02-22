92ad83509033a83a19050851f21e63aeca393ed8
Hans Harder <asdfsolider@googlemail.com>
2019-02-22 21:15:13 +0100

    formatierung

--- lib/Utilities/descriptable.hpp
+++ lib/Utilities/descriptable.hpp
@@ -5,15 +5,36 @@ template <typename T>
 class Descriptable
 {
   private:
-    String description;
-    T content;
-  public:
-    Descriptable(String description) { this->description = description; }
-    String getDescription() {return this->description;}
-    T getContent() {return this->content;}
-    void set(T content) {this->content = content;}
-    Descriptable<T> &operator=(const T &other) { 
-        set(other); 
-        return *this;
-    };
+	String description, unit;
+	T content;
+
+  public:	
+	Descriptable(String description, String unit)
+	{
+		this->description = description;
+		this->unit = unit;
+	}
+
+	Descriptable(String description) : Descriptable(description, "") {}
+
+	T getContent()
+	{
+		return this->content;
+	}
+
+	void set(T content)
+	{
+		this->content = content;
+	}
+
+	Descriptable<T> &operator=(const T &other)
+	{
+		set(other);
+		return *this;
+	}
+
+	String str()
+	{
+		return description + " " + String(content) + unit;
+	}
 };
